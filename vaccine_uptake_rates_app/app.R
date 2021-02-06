b2e01635e8c7ffa769985bc88b650c443e22421c
adisarid <adi@sarid-ins.co.il>
2021-02-06 23:26:48 +0200

    updated app

--- vaccine_uptake_rates_app/app.R
+++ vaccine_uptake_rates_app/app.R
@@ -44,7 +44,9 @@ create_vaccination_data <- function(cities_select = c("תל אביב - יפו",
         geom_line(size = 1) + 
         saridr::theme_sarid() + 
         ylab("Population [%]") + 
-        ggtitle("\u202bחיסוני COVID19 בישראל לפי רשויות") +
+        ggtitle("\u202bחיסוני COVID19 בישראל לפי רשויות",
+                subtitle = paste0("\u202bקבוצות גיל: ", 
+                                  paste0(age_group_select, collapse = ";"))) +
         scale_y_continuous(labels = scales::percent_format(1)) + 
         guides(color = guide_legend("עיר")) +
         labs(caption = "\u202bמקור הנתונים: מאגר COVID-19 של משרד הבריאות מתחסנים על פי יישוב\n\u202bויז'ואליזציה מכון שריד: https://www.sarid-ins.co.il") + 
@@ -80,7 +82,13 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                                               icon = icon("refresh"))
                                  ),
                     mainPanel(width = 8,
-                              plotOutput("out_chart")
+                              h1("Vaccination rates in Israel"),
+                              plotOutput("out_chart"),
+                              h4("Sources:"),
+                              HTML("<p>Town population by age from <a href='https://data.gov.il/dataset/residents_in_israel_by_communities_and_age_groups/resource/64edd0ee-3d5d-43ce-8562-c336c24dbc1f'>MOI</a></p>"),
+                              HTML("<p>Vaccination rates from <a href='https://data.gov.il/dataset/covid-19/resource/12c9045c-1bf4-478a-a9e1-1e876cc2e182'>MOH</a></p>"),
+                              HTML("<p>App by <a href='https://www.sarid-ins.co.il'>Sarid Research Institute</a></p>"),
+                              img(src = "logo_eng_2.png", width = "150px")
                     )
                 )
 )
@@ -90,17 +98,16 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
 
 server <- function(input, output) {
     
+    # create the plot
     output$out_chart <- renderPlot({
         
-        # require the refresh button to be clicked before rendering
-        req(input$refresh_button)
+        input$refresh_button
         
-        # use the function to render the plot
         create_vaccination_data(cities_select = isolate(input$choose_cities),
                                 dose_select = isolate(input$dose),
                                 age_group_select = isolate(input$age_group_select))
-        
     })
+    
 }
 
 shinyApp(ui = ui, server = server)
